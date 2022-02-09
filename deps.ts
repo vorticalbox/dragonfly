c80491eaff56c9d16efe4aae93200d0e09ac7fc8
vorticalbox <vorticalbox@protonmail.com>
2022-02-09 13:39:16 +0000

    update: oak

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * as denodb from 'https://deno.land/x/denodb@v1.0.40/mod.ts';
-export * as oak from 'https://deno.land/x/oak@v10.1.0/mod.ts';
+export * as oak from 'https://deno.land/x/oak@v10.2.0/mod.ts';
 export { z } from 'https://deno.land/x/zod@v3.11.6/mod.ts'
