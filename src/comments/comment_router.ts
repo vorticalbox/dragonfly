f5dc404f3f8ee4e0365aa1f89683c172f5442e2d
vorticalbox <vorticalbox@protonmail.com>
2022-07-15 10:00:45 +0100

    add: post_id validtion to comment route

--- src/comments/comment_router.ts
+++ src/comments/comment_router.ts
@@ -1,6 +1,7 @@
 import { CTX } from "../types/oak.ts";
 import parseQuery from '../utils/query.ts';
-import * as comments from './comment_service.ts'
+import * as comment_service from './comment_service.ts'
+import * as post_service from '../posts/post_service.ts'
 import { oak } from '../deps.ts'
 
 const { helpers } = oak;
@@ -8,16 +9,17 @@ export default function (router: oak.Router) {
     router.get('/comment', async (ctx) => {
         const { skip, limit } = parseQuery(ctx)
         const { post_id } = helpers.getQuery(ctx, { mergeParams: true });
-        if (!post_id) {
+        const post = await post_service.get_post(post_id)
+        if (!post) {
             ctx.response.status = 404;
             ctx.response.body = 'Invalid post_id';
             return;
         }
-        ctx.response.body = await comments.getComments(post_id, skip, limit)
+        ctx.response.body = await comment_service.getComments(post_id, skip, limit)
     });
     router.post('/comment', async (ctx: CTX) => {
         const { post_id } = helpers.getQuery(ctx, { mergeParams: true });
         const comment = await ctx.request.body({ type: "json" }).value;
-        ctx.response.body = await comments.addComment({ post_id, ...comment }, ctx.state.user.username)
+        ctx.response.body = await comment_service.addComment({ post_id, ...comment }, ctx.state.user.username)
     });
 }
