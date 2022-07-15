7780078e7489620dd4beffd39879614658ac1859
vorticalbox <vorticalbox@protonmail.com>
2022-07-15 10:00:07 +0100

    correct username validation

--- src/users/user_validation.ts
+++ src/users/user_validation.ts
@@ -2,7 +2,7 @@ import { z } from "../deps.ts";
 
 export const register_schema = z.object({
   username: z.string()
-    .max(150, 'username must not be longer then 50 characters'),
+    .max(50, 'username must not be longer then 50 characters'),
   password: z.string()
     .min(7, 'title must be at least 7 characters long')
 })
